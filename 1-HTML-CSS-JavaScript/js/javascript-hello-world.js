e0a255593a23b8d61e37ab0e6c11525a2643d2f4
Eric Renshaw <rens0047@umn.edu>
2021-08-19 20:55:24 -0500

    Second commit

---
+++ 1-HTML-CSS-JavaScript/js/javascript-hello-world.js
@@ -0,0 +1,9 @@
+function whatName() {
+    const tellMeName = prompt(' Tell Me your name');   
+     sayHi(tellMeName);
+    }
+ 
+ function sayHi(gF) {
+    alert(' nice to meet you ' + gF + ' Dude' );
+ }
+ 
